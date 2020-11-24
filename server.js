ed7dcb43e75acb82314bf7c12d17a9af25637731
Aseelsamer <assoul_08@hotmail.com>
2020-11-24 18:08:18 +0200

    solving issues

--- server.js
+++ server.js
@@ -1,111 +1,154 @@
-'use strict';
-// Application Dependencies
-const express = require('express');
-//CORS = Cross Origin Resource Sharing
-const cors = require('cors');
-//DOTENV (read our enviroment variable)
-require('dotenv').config();
-const superagent = require('superagent');
-// Application Setup
-const PORT = process.env.PORT || 3030;
-const app = express();
-app.use(cors());
-//Routes
-app.get('/location', locationHandlerFunc);
-app.get('/weather', getWeather);
-app.get('/trails', trailsHandler);
-app.get('*', notFoundPageHandler);
-app.use(errorHandler);
-// https://city-explorer-backend.herokuapp.com/location?city=amman
-function locationHandlerFunc(req, res) {
-    // ? -> query string parameteres
-    let cityName = req.query.city;
-    // to get data from api server -> request url + key
-    let locKey = process.env.LOCATION_KEY;
-    let url = `https://eu1.locationiq.com/v1/search.php?key=${locKey}&q=${cityName}&format=json`;
-    // superagent: client-side HTTP request library
-    // console.log('before the superagent');
-    superagent.get(url)
-        .then(data => {
-            // console.log('inside the superagent');
-            // console.log(data.body);
-            let locationData = new Location(cityName, data.body);
-            res.send(locationData);
-        })
-        .catch(() => {
-            errorHandler('Location .. Something went wrong!!', req, res);
-        });
-    // console.log('after the superagent');
-}
-function Location(city, geoData) {
-    this.search_query = city;
-    this.formatted_query = geoData[0].display_name;
-    this.latitude = geoData[0].lat;
-    this.longitude = geoData[0].lon;
-}
-//  creating a function for location
-function getWeather(req, res) {
-    console.log(req.query);
-//   let city = req.query.search_query;
-  let cityName = req.query.search_query;
-  let lat = req.query.latitude;
-  let lon = req.query.longitude;
-  let wthrKye = process.env.WETHER_KEY;
-  let weatherDaily =[];
-  let days =5;
-  let weatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?city=${cityName}&lat=${lat}&lon=${lon}&key=${wthrKye}&days=${days}`;
 
-//   let weatherDescription = [];
-  // let jsonData = require('./data/weather.json');
-  superagent.get(weatherUrl)
-  .then(data => {
-    //   console.log(data);
-   weatherDaily = data.body.data.map(val => {
-       return new Weather(val);
+'use strict';
+
+const express = require('express');
+require('dotenv').config();
+const server = express();
+const pg = require('pg');
+const client = new pg.Client(process.env.DATABASE_URL);
+const superagent = require('superagent');
+// cors added
+const cors = require('cors');
+server.use(cors());
+
+
+const PORT = process.env.PORT;
+
+server.get('/', (req, res) => {
+  res.send('Homepage');
+});
+
+server.get('/location', locationHandler);
+server.get('/weather', weatherHandler);
+server.get('/trails', trailHandler);
+
+function locationHandler(req, res) {
+
+  let cityName = req.query.city;
+  let locationToken = process.env.GEO_API_KEY;
+  let url = `https://us1.locationiq.com/v1/search.php?key=${locationToken}&q=${cityName}&format=json`;
+
+  let SQL = `SELECT * FROM location WHERE search_query = '${cityName}'`;
+  client.query(SQL)
+    .then(result => {
+      if (result.rowCount !== 0) {
+        res.status(200).json(result.rows);
+      } else if (result.rowCount === 0) {
+        console.log('inside else');
+        callLocationAPI(url, cityName, res)
+          .then(locData => {
+            console.log(locData);
+            res.status(200).json(locData);
+          });
+        console.log('after callback function');
+      }
+    })
+    .catch(error => errorHandler(error, req, res));
+
+  /*superagent.get(url)
+    .then(data => {
+      const locationObject = new Location(cityName, data.body);
+      res.status(200).send(locationObject);
+    })
+    .catch(()=> {
+      errorHandler('Location .. Something went wrong!!', req, res);
+    });*/
+}
+function callLocationAPI(url, cityName, res) {
+  superagent.get(url)
+    .then(data => {
+      console.log('inside callback function');
+      const locationObject = new Location(cityName, data.body);
+      let insertSQL = `INSERT INTO location (search_query,formatted_query, latitude, longitude) VALUES ($1,$2,$3,$4)`;
+      let safeValues = [locationObject.search_query, locationObject.formatted_query, locationObject.latitude, locationObject.longitude];
+      client.query(insertSQL, safeValues)
+        .then(() => {
+          console.log('your data has been added successfully!!');
+        });
+      console.log(locationObject);
+      return locationObject;
     });
-    res.send(weatherDaily);
-}).catch(() => {
-    // res.status(500).send('Sorry, something went wrong');
-    errorHandler('Weather .. Something went wrong!!', req, res);
-  });
 }
-function Weather(weatheData) {
-  this.forecast = weatheData.weather.description;;
-  this.time =  weatheData.datetime;
-}
-function trailsHandler(req, res) {
-  let lat = req.query.latitude;
-  let lon = req.query.longitude;
-  let triKye = process.env.TRAIL_KEY;
-  let triUrl = `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=200&key=${triKye}`;
-  superagent.get(triUrl)
-  .then(data => {
-    let availableTrails = data.body.trails.map(val => {
-      return new Trail(val);
+
+function weatherHandler(req, res) {
+  let cityName = req.query.search_query;
+  let cityLat = req.query.lat;
+  let cityLon = req.query.lon;
+
+  console.log(req.query);
+  let weatherKey = process.env.WEATHER_API_KEY;
+  let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${cityName}&key=${weatherKey}&days=8&lat=${cityLat}&lon=${cityLon}`;
+  superagent.get(url)
+    .then(weatehrData => {
+      let weatherObjcts = weatehrData.body.data.map(w => {
+        let weatherObject = new Weather(w);
+        return weatherObject;
+      });
+      res.status(200).json(weatherObjcts);
+    })
+    .catch(() => {
+      errorHandler('Weather .. Something went wrong!!', req, res);
     });
-    res.status(200).send(availableTrails);
-  }).catch(() => {
-    errorHandler('Trail .. Something went wrong!!', req, res);
-  });
 }
-function Trail(data) {
-  this.name = data.name;
-  this.location = data.location;
-  this.length = data.length;
-  this.stars = data.stars;
-  this.star_votes = data.starVotes;
-  this.summary = data.summary;
-  this.trail_url = data.url;
-  this.conditions = data.conditionStatus;
-  this.condition_date = data.conditionDate.split(' ')[0];
-  this.condition_time = data.conditionDate.split(' ')[1];
+
+
+function trailHandler(req, res) {
+  let trailLat = req.query.latitude;
+  let trailLon = req.query.longitude;
+  let trailsKey = process.env.TRAIL_API_KEY;
+  let url = `https://www.hikingproject.com/data/get-trails?lat=${trailLat}&lon=${trailLon}&maxDistance=10&key=${trailsKey}`;
+
+  superagent.get(url)
+    .then(tarilsData => {
+      let trailObjects = tarilsData.body.trails.map(t => {
+        let trail = new Trail(t);
+        return trail;
+      });
+      res.status(200).json(trailObjects);
+    })
+    .catch(() => {
+      errorHandler('Trails .. Something went wrong!!', req, res);
+    });
 }
-function notFoundPageHandler(req, res) {
-    res.status(404).send('Not Found');
-}
+
 function errorHandler(error, req, res) {
-    res.status(500).send(error);
+  res.status(500).send(error);
 }
-app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`)
-})
+
+function Location(city, locationData) {
+  this.search_query = city;
+  this.formatted_query = locationData[0].display_name;
+  this.latitude = locationData[0].lat;
+  this.longitude = locationData[0].lon;
+}
+
+function Weather(weatehrData) {
+  this.forecast = weatehrData.weather.description;
+  this.time = weatehrData.datetime;
+}
+function Trail(trailData) {
+  this.name = trailData.name;
+  this.location = trailData.location;
+  this.length = trailData.length;
+  this.stars = trailData.stars;
+  this.star_votes = trailData.starVotes;
+  this.summary = trailData.summary;
+  this.trail_url = trailData.url;
+  this.conditions = trailData.conditionStatus;
+  this.condition_date = trailData.conditionDate.split(' ')[0];
+  this.condition_time = trailData.conditionDate.split(' ')[1];
+}
+server.get('*', (req, res) => {
+  res.status(400).send('Not found');
+});
+server.use((error, req, res) => {
+  res.status(500).send('Sorry, something went wrong');
+});
+
+
+client.connect()
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Listening on port ${PORT}`);
+    });
+  });
